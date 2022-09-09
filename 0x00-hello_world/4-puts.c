d51b0d9b7e6442fd6a66dc804ae44a26cc759159
Erinkitol Mislat <erinkitolamislat@gmail.com>
2022-09-08 17:35:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main-Entry point
+ * Return: 0 if successful
+ */
+ int main(void)
+ {
+ puts("\"Programming iss like building a multilingual puzzle"
+ );
+ return (0);
+ }
